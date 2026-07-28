e83a725e743d7b31799cb1c9c73f50ea5d6c405c
rajazxc <dj8156260@gmail.com>
2026-07-28 14:19:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,5 +15,8 @@ client = discord.Client(intents=intents)
 @client.event
 async def on_ready():
     print(f"Logged in as {client.user}")
+print(repr(DISCORD_TOKEN))
+print(len(DISCORD_TOKEN))
 
+client.run(DISCORD_TOKEN)
 client.run(DISCORD_TOKEN)
